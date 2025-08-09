8c9e03091ea8426ff606637e291621a78d09a1d8
mohammedtohamym <mohammedtohamym@gmail.com>
2025-08-09 15:58:58 +0300

    chore: add app colors

---
+++ lib/core/resources/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/animation.dart';
+
+abstract class AppColors {
+  static const Color primaryOrange = Color(0xFFF6B318);
+  static const Color secondaryRed = Color(0xFF9A3324);
+  static const Color body25 = Color(0xFFFFFFFF);
+  static const Color body100 = Color(0xFFF2F3F6);
+  static const Color body200 = Color(0xFFF5F4F4);
+  static const Color body300 = Color(0xFFD9D9D9);
+  static const Color body700 = Color(0xFF757575);
+  static const Color body900 = Color(0xFF2D2A26);
+}
