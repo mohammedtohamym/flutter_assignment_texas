15984e1a959ec70ff00be1fd2a90431016d97f76
mohammedtohamym <mohammedtohamym@gmail.com>
2025-08-09 16:00:40 +0300

    chore: add app fonts

---
+++ lib/core/resources/app_fonts.dart
@@ -0,0 +1,12 @@
+class AppFonts {
+  static const String _bernier = "BERNIER";
+  static const String _specialGothicCondensedOne = "SpecialGothicCondensedOne";
+
+  static String getBernier() {
+    return _bernier;
+  }
+
+  static String getSpecialGothicCondensedOne() {
+    return _specialGothicCondensedOne;
+  }
+}
