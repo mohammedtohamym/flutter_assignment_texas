c0b6c8520163450b882edf215c414a55ace37f97
mohammedtohamym <mohammedtohamym@gmail.com>
2025-08-09 16:38:33 +0300

    chore: add app theme

---
+++ lib/app/app_theme.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_assignment_texas/core/resources/app_colors.dart';
+import 'package:flutter_assignment_texas/core/resources/app_fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+ThemeData getAppTheme(BuildContext context) {
+  return ThemeData(
+    primaryColor: AppColors.primaryOrange,
+    scaffoldBackgroundColor: Colors.white,
+    splashColor: AppColors.primaryOrange.withValues(alpha: 0.1.r),
+    highlightColor: AppColors.primaryOrange.withValues(alpha: 0.1.r),
+    // app bar
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      foregroundColor: Colors.white,
+      titleTextStyle: TextStyle(
+        color: AppColors.body900,
+        fontFamily: AppFonts.getBernier(),
+      ),
+    ),
+
+    // elevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        backgroundColor: WidgetStateProperty.all<Color>(
+          AppColors.primaryOrange,
+        ),
+        shape: WidgetStateProperty.all(
+          RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
+        ),
+        foregroundColor: WidgetStateProperty.all<Color>(AppColors.body900),
+        textStyle: WidgetStateProperty.all<TextStyle>(
+          TextStyle(fontFamily: AppFonts.getSpecialGothicCondensedOne()),
+        ),
+      ),
+    ),
+
+    // Text Theme
+    textTheme: Theme.of(context).textTheme.apply(
+      bodyColor: AppColors.body900,
+      fontFamily: AppFonts.getBernier(),
+    ),
+  );
+}
