9b6075ab30ac108c0befe90250ae1269175aef2f
mohammedtohamym <mohammedtohamym@gmail.com>
2025-08-09 16:04:30 +0300

    chore: add app icons

---
+++ lib/core/resources/app_assets.dart
@@ -0,0 +1,34 @@
+abstract class AppAssets {
+  //menu icons
+  static const String addIcon = 'assets/icons/add_icon.svg';
+  static const String trashIcon = 'assets/icons/trash_icon.svg';
+  static const String favoriteIcon = 'assets/icons/favorite_icon.svg';
+
+  //general icons
+  static const String searchIcon = 'assets/icons/search_icon.svg';
+  static const String backIcon = 'assets/icons/back_icon.svg';
+  static const String rightArrowIcon = 'assets/icons/right_arrow_icon.svg';
+
+  //profile icons
+  static const String addressIcon = 'assets/icons/address_icon.svg';
+  static const String notificationsIcon = 'assets/icons/notifications_icon.svg';
+  static const String offersIcon = 'assets/icons/offers_icon.svg';
+  static const String passwordIcon = 'assets/icons/password_icon.svg';
+  static const String communicationIcon = 'assets/icons/communication_icon.svg';
+  static const String marketingIcon = 'assets/icons/marketing_icon.svg';
+  static const String dealsIcon = 'assets/icons/deals_icon.svg';
+  static const String deleteAccountIcon =
+      'assets/icons/delete_account_icon.svg';
+  static const String logoutIcon = 'assets/icons/logout_icon.svg';
+
+  // navigation bar icons
+  static const String homeIcon = 'assets/icons/home_icon.svg';
+  static const String menuIcon = 'assets/icons/menu_icon.svg';
+  static const String moreIcon = 'assets/icons/more_icon.svg';
+  static const String profileIcon = 'assets/icons/profile_icon.svg';
+  static const String texasLogoIcon = 'assets/icons/texas_logo_icon.svg';
+  static const String cartBagIcon = 'assets/icons/cart_bag_icon.svg';
+
+  //mock data - static for the task
+  static const String egyptFlagIcon = 'assets/icons/Egypt_flag_icon.svg';
+}
