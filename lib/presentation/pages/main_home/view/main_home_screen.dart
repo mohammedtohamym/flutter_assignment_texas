1ca96059bd68308cc698d60ba1dd9ebffd0786c3
mohammedtohamym <mohammedtohamym@gmail.com>
2025-08-10 15:31:43 +0300

    feat: implement main home screen and app routes

---
+++ lib/presentation/pages/main_home/view/main_home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MainHomeScreen extends StatelessWidget {
+  const MainHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
