814a3a57051e4ee97df1b107c1daaee77553e358
mohammedtohamym <mohammedtohamym@gmail.com>
2025-08-10 15:48:42 +0300

    feat: implement Cart, Home, Menu, More, and Profile screens with initial structure

---
+++ lib/presentation/pages/main_home/tabs/menu/view/menu_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
